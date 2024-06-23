84ee1e6407c59a777ca412723bdf61393b4d524d
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-06-23 17:01:54 +0300

    Grid for the game implemented

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -16,9 +16,36 @@ namespace SnakeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetUpGrid();
+        }
+
+        private Image[,] SetUpGrid()
+        {
+            Image[,] images = new Image[rows, cols];    
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for(int r = 0; r < rows; r++)
+            {
+                for(int c = 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                } 
+            }
+
+            return images;
         }
     }
 }
