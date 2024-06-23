c39fa93b9fc5fdf8a8da28a82e9685c7c05f86a2
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-06-23 12:53:38 +0300

    Working on game logic implementation

---
+++ SnakeGame/GameState.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class GameState
+    {
+        //current state of the game
+        public int Rows { get; }
+        public int Columns { get; }
+        public GridValue[,] Grid { get; }
+
+        public DirectionGrid Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        //list with positions currently occupied by the snake
+        private readonly LinkedList<PositionGrid> snakePositions = new LinkedList<PositionGrid>();
+        //where the food should spawn
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols) 
+        { 
+            Rows = rows;
+            Columns = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = DirectionGrid.Right;
+
+            AddSnake();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+            for(int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new PositionGrid(r, c));
+            }
+        }
+    }
+}
--- SnakeGame/PositionGrid.cs
+++ SnakeGame/PositionGrid.cs
@@ -1,4 +1,5 @@
-﻿namespace SnakeGame
+﻿
+namespace SnakeGame
 {
     public class PositionGrid
     {
@@ -15,6 +16,28 @@
         {
             //moving one step in the given direction
             return new PositionGrid(Row + dir.RowOffSet, Col + dir.ColumnOffSet);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PositionGrid grid &&
+                   Row == grid.Row &&
+                   Col == grid.Col;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Col);
+        }
+
+        public static bool operator ==(PositionGrid left, PositionGrid right)
+        {
+            return EqualityComparer<PositionGrid>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(PositionGrid left, PositionGrid right)
+        {
+            return !(left == right);
         }
     }
 }
